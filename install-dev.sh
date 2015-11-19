c6d51263c48cbecaad98cef063b2416ca5a6e195
Dane Springmeyer <dane@mapbox.com>
2015-11-19 14:41:41 -0800

    add script comments [skip ci]

--- install-dev.sh
+++ install-dev.sh
@@ -3,6 +3,20 @@
 set -e -u
 set -o pipefail
 
+: '
+
+This script allows us to install the devDependencies of a given module
+after it has already been installed into `./node_modules`. The reason
+this script exists is that we want to:
+
+1) Install modules from github
+2) Be able to run their tests
+3) Which means we need their devDependencies
+4) But the devDependencies are not installed by default
+
+Anyone have better ideas of how to do this?
+
+'
 for i in $(ls node_modules/); do
   (
     cd node_modules/$i
